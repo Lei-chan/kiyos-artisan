30d37537f5a44e8f0542b281da29e8aaf49ae5b1
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-01-19 00:33:08 +0900

    Updated for debugging

--- src/app/[locale]/components/TopBar.tsx
+++ src/app/[locale]/components/TopBar.tsx
@@ -63,7 +63,7 @@ function LanguageSelect({ currentLocale }: { currentLocale: TYPE_LOCALE }) {
 
   return (
     <select
-      className="w-5 h-full bg-transparent bg-[url('/icons/globe.svg')] bg-contain bg-center bg-no-repeat appearance-none -webkit-appearance-none text-sm  text-blue-900/0 focus:text-black cursor-pointer"
+      className="w-5 h-full bg-transparent bg-[url('/icons/globe.svg')] bg-contain bg-center bg-no-repeat appearance-none -webkit-appearance-none text-sm  text-blue-900 opacity-0 focus:text-black focus:opacity-100 cursor-pointer"
       value={currentLocale}
       onChange={handleChangeLanguage}
     >
